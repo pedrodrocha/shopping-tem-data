855572ca5d186f7cbb3113ca65ab0f3a4fdc6cbc
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2023-06-24 14:33:01 -0300

    installing sql alchemy + alembic

--- testing.py
+++ testing.py
@@ -1,8 +1,1 @@
-import json
-
-with open('raw-data/eldorado.json', encoding="utf8") as user_file:
-  file_contents = user_file.read()
-  
-parsed_json = json.loads(file_contents)
-
-print(parsed_json['data'][0])
+print('Hey, you')
